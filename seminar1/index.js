2e414a1b93159651777826a367eb16e05112034a
ciobanoiu andreea elena <andreeaelena@MacBook-Air-ciobanoiu.local>
2025-10-08 20:09:16 +0300

    hello

---
+++ seminar1/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res) => {
+    res.send('hello world')
+})
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+})
+  
+
+app.listen(3000)
--- seminar1/test.js
+++
@@ -1,9 +0,0 @@
-const http = require ('http');
-
-http
-  .createServer(function (req, res) {
-    console.log('listening on port 3000...');
-    res.write("hello web");
-    res.end();
-  })
-  .listen(3000);
